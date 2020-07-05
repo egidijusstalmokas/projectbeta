de569036ffca4d2fbb3e2c69c2c7e1a54c9fb4f6
Egidijus Stalmokas <egidijusstalmokas96@gmail.com>
2020-07-05 16:56:17 +0300

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -87,8 +87,11 @@
                 <div class="links">
                     <a href="https://laravel.com/docs">Docs</a>
                     <a href="https://laracasts.com">Laracasts</a>
+                    <a href="https://laravel.com/docs">Do</a>
+                    <a href="https://laracasts.com">Laraca</a>
                     <a href="https://laravel-news.com">News</a>
                     <a href="https://blog.laravel.com">Blog</a>
+                    
                     <a href="https://nova.laravel.com">Nova</a>
                     <a href="https://forge.laravel.com">Forge</a>
                     <a href="https://vapor.laravel.com">Vapor</a>
